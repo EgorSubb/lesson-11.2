248d62cd94a1188520ea5b295bbd69d1f8e30329
Egor Subbotin <egorsubbotin1997@gmail.com>
2023-03-30 16:35:55 +0500

    Add new def and docstring in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,5 +1,9 @@
 def input_upper(user_input):
+    """Делает буквы заглавными"""
     word_upper = user_input.upper()
     return word_upper
 
-print(input_upper("ds ds sd ds"))
+def words_upper(str_input):
+    """Делает первую букву каждого слова заглавной"""
+    text = str_input.title()
+    return text
